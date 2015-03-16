b5dccd1294a404985aa456102bf08b086fb89f2f
Vasyl Purchel <vasyl.purchel@gmail.com>
2015-03-16 22:13:46 +0000

    Removed unnecessary 'puts'

--- problem1.rb
+++ problem1.rb
@@ -1,6 +1,6 @@
-puts 'Problem 1: Multiples of 3 and 5'
-puts '==============================='
 puts <<EOF
+Problem 1: Multiples of 3 and 5
+======================================
 If we list all the natural numbers
 below 10 that are multiples of 3 or 5,
 we get 3, 5, 6 and 9.
@@ -8,8 +8,8 @@ The sum of these multiples is 23.
 
 Find the sum of all the multiples
 of 3 or 5 below 1000.
+======================================
 EOF
-puts '==============================='
 
 def sum_of_multiples_of_3_and_5_below(number)
   sum, i = 0, 3
--- problem3.rb
+++ problem3.rb
@@ -1,13 +1,13 @@
-puts 'Problem 3: Largest prime factor'
-puts '==============================='
 puts <<EOF
+Problem 3: Largest prime factor
+===================================
 The prime factors of 13195 are
 5, 7, 13 and 29.
 
 What is the largest prime factor of
 the number 600851475143 ?
+===================================
 EOF
-puts '==============================='
 
 @primes = []
 def prime?(number)
